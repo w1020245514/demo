56a519aabeb07ad5c97f1da479c0fb26fd723ee7
w1020245514 <31184039+w1020245514@users.noreply.github.com>
2019-12-08 21:27:32 +0800

    Update DataSourceConfiguration.java

--- src/main/java/com/moli/demo/config/dao/DataSourceConfiguration.java
+++ src/main/java/com/moli/demo/config/dao/DataSourceConfiguration.java
@@ -34,7 +34,7 @@ public class DataSourceConfiguration {
         // 生成datasource实例
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         // 跟配置文件一样设置以下信息
-        // 驱动
+        // 设置驱动
         dataSource.setDriverClass(jdbcDriver);
         // 数据库连接URL
         dataSource.setJdbcUrl(jdbcUrl);
@@ -43,7 +43,7 @@ public class DataSourceConfiguration {
         // 设置用户密码
         dataSource.setPassword(jdbcPassword);
         // 配置c3p0连接池的私有属性
-        // 连接池最大线程数
+        // 配置连接池最大线程数
         dataSource.setMaxPoolSize(30);
         // 连接池最小线程数
         dataSource.setMinPoolSize(10);
